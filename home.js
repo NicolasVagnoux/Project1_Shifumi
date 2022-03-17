4aa9972b90cd463f01599a8f3ac6615ec07f6d31
Nicolas Vagnoux <nicolas.vagnoux@gmail.com>
2022-03-17 17:21:47 +0100

    Finished desktop homepage

--- home.js
+++ home.js
@@ -2,6 +2,7 @@
 
 const navBar = document.querySelector('nav');
 const logoImg = document.querySelector('#navlogo');
+const navText = document.querySelector('.navbar__list');
 
 window.onscroll = function() {myScroll()};
 
@@ -9,9 +10,13 @@ function myScroll() {
   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
     navBar.style.backgroundColor = "rgba(0, 0, 0, 0.75)";
     logoImg.style.width = "60px";
+    navText.style.fontSize = "1.6rem";
+    navBar.style.padding = "0px";
   } else {
     navBar.style.backgroundColor = "rgba(255, 255, 255, 0)";
     logoImg.style.width = "100px";
+    navText.style.fontSize = "2.2rem";
+    navBar.style.padding = "16px";
   }
 }
 
